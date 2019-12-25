7bb46d6acf7a3331a3158d3fc23b446cc78a0374
hoangdat1020 <hoangdat1020@gmail.com>
2019-12-25 16:13:11 +0700

    Design HomeUI

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,18 @@
 /* eslint-disable react/no-unused-state */
 /* eslint-disable react/state-in-constructor */
 import React, {Component} from 'react';
-import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
+import {
+  StyleSheet,
+  View,
+  ScrollView,
+  Text,
+  ImageBackground,
+  Dimensions
+} from 'react-native';
+import Icon from 'react-native-ionicons';
+import moment from 'moment';
 
+const {width} = Dimensions.get('window');
 const date = '2019-03-28';
 
 export default class Home extends Component {
@@ -28,10 +38,11 @@ export default class Home extends Component {
       .then(data2 => {
         this.setState({
           name: data2.name,
-          humidity: data2.main.humidity,
           temp: data2.main.temp,
           weather: data2.weather[0].main,
-          descrip: data2.weather[0].description
+          descrip: data2.weather[0].description,
+          tempMax: data2.main.temp_max,
+          tempMin: data2.main.temp_min
         });
       });
   }
@@ -55,17 +66,46 @@ export default class Home extends Component {
   }
 
   render() {
-    console.log(this.state.list);
+    console.log('date: ', moment().format('dddd'));
+    console.log('2010-10-20 04:30:00'.slice(11, 13));
+    //console.log(this.state.list);
     if (this.props.navigation.getParam('citySearch')) {
       this.getWeatherCity();
     }
     return (
-      <View style={styles.container}>
-        <Text>name: {this.state.name} </Text>
-        <Text>temp: {this.state.temp} </Text>
-        <Text>weather: {this.state.weather} </Text>
-        <Text>descrip: {this.state.descrip} </Text>
-        <Text>humidity: {this.state.humidity} </Text>
+      <View>
+        <ImageBackground
+          source={require('../assets/bg.png')}
+          style={{width: '100%', height: '100%'}}>
+          <View style={styles.viewTextCity}>
+            <Text style={styles.textCity}>{this.state.name}</Text>
+            <Text style={styles.textMain}>{this.state.weather}</Text>
+          </View>
+          <ScrollView>
+            <View style={styles.container}>
+              <View style={styles.viewTop}>
+                <Text style={styles.textTemp}>{this.state.temp}</Text>
+                <Text style={styles.textCirle}>°</Text>
+              </View>
+              <View style={styles.viewtextSpaceBetween}>
+                <View>
+                  <Text style={styles.textSpaceBetWeen}>
+                    Today {moment().format('dddd')}
+                  </Text>
+                </View>
+                <View>
+                  <Text style={styles.textSpaceBetWeen}>
+                    {this.state.tempMin} {this.state.tempMax}
+                  </Text>
+                </View>
+              </View>
+              <ScrollView
+                style={styles.scrollViewHorizontal}
+                showsHorizontalScrollIndicator={false}
+                horizontal></ScrollView>
+            </View>
+          </ScrollView>
+        </ImageBackground>
       </View>
     );
   }
@@ -75,10 +115,143 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+    alignItems: 'center'
+  },
+  viewTop: {
+    backgroundColor: 'transparent',
+    justifyContent: 'center',
+    alignContent: 'center',
+    alignItems: 'center'
+  },
+  textCity: {
+    position: 'absolute',
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 25,
+    fontWeight: 'bold',
+    top: 70
+  },
+  textMain: {
+    position: 'absolute',
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 16,
+    top: 100
+  },
+  textTemp: {
+    color: 'white',
+    fontSize: 85
+  },
+  textCirle: {
+    fontSize: 80,
+    color: 'white',
+    position: 'absolute',
+    top: -5,
+    right: -35
+  },
+  viewtextSpaceBetween: {
+    flexDirection: 'row',
+    width,
+    paddingHorizontal: 10,
+    justifyContent: 'space-between',
+    marginTop: 50
+  },
+  textSpaceBetWeen: {
+    color: 'white',
+    fontSize: 15
+  },
+  scrollViewHorizontal: {
+    marginTop: 10,
+    width,
+    height: 120,
+    borderBottomColor: 'white',
+    borderBottomWidth: 1,
+    borderTopColor: 'white',
+    borderTopWidth: 1
+  },
+  viewDayOfWeek: {
+    width,
+    height: 400,
+    borderBottomColor: 'white',
+    borderBottomWidth: 1
+  },
+  infoDay: {
+    width,
+    height: 100,
+    borderBottomColor: 'white',
+    borderBottomWidth: 1
+  },
+  detailsDay: {
+    justifyContent: 'center',
     alignItems: 'center',
-    paddingTop: 16,
-    paddingLeft: 16,
-    paddingRight: 16,
-    backgroundColor: '#E6E6E6'
+    width,
+    height: 300,
+    borderBottomColor: 'white',
+    borderBottomWidth: 1
+  },
+  rowDetails: {
+    width: width - 50,
+    height: 60,
+    borderBottomColor: 'white',
+    borderBottomWidth: 1,
+    paddingHorizontal: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  viewTextCity: {
+    width,
+    height: 120,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'transparent'
+  },
+  vtextScrollViewHorizontal: {
+    flexDirection: 'row'
+  },
+  textScrollViewHorizontal: {
+    padding: 10,
+    color: 'white'
+  },
+  viconScrollViewHorizontal: {
+    flexDirection: 'row',
+    marginLeft: 10
+  },
+  textBottomScrollViewHorizontal: {
+    paddingLeft: 15,
+    color: 'white'
+  },
+  viewDay: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  viewBetWeen: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  textBetWeen: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 20
+  },
+  alignLeftText: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  textTitle: {
+    color: 'white'
+  },
+  value: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold'
   }
 });
+
+//
+// <View style={styles.container}>
+//   <Text>name: {this.state.name} </Text>
+//   <Text>temp: {this.state.temp} </Text>
+//   <Text>weather: {this.state.weather} </Text>
+//   <Text>descrip: {this.state.descrip} </Text>
+//   <Text>humidity: {this.state.humidity} </Text>
+// </View>
