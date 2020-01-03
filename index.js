0e9c7473caaea05aae565dfcac919476d500dee2
hoangdat1020 <hoangdat1020@gmail.com>
2020-01-03 13:37:09 +0700

    fix Firebase

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import {AppRegistry} from 'react-native';
 // import App from './src/navigator/App';
 // import Search from './src/components/Search';
 import Home from './src/components/firebaseLogin/App';
-import AppNavi from './src/navigator/AppNavigation';
+// import AppNavi from './src/navigator/AppNavigation';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => Home);
